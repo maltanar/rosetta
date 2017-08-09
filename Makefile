52ee6fcb0b8893b460e1e5de3a65adc3dc1f3165
Yaman Umuroglu <maltanar@gmail.com>
2017-08-09 18:07:48 +0200

    more Makefile targets

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 SBT ?= sbt
 SBT_FLAGS ?= -Dsbt.log.noformat=true
 CC = g++
-TOP ?= .
+TOP ?= $(shell readlink -f .)
 BUILD_DIR ?= $(TOP)/build
 BUILD_DIR_VERILOG := $(BUILD_DIR)/hw/verilog
 BUILD_DIR_HWCPP := $(BUILD_DIR)/hw/cpp_emu
 BUILD_DIR_HWDRV := $(BUILD_DIR)/hw/driver
+BUILD_DIR_EMULIB_CPP := $(BUILD_DIR)/hw/cpp_emulib
+DRV_SRC_DIR := $(TOP)/src/main/cpp/regdriver
+VIVADO_PROJ_SCRIPT := $(TOP)/src/main/script/make-vivado-project.tcl
+
+
+HW_VERILOG := $(BUILD_DIR_VERILOG)/PYNQWrapper.v
+BITFILE_PRJNAME := bitfile_synth
+BITFILE_PRJDIR := $(BUILD_DIR)/bitfile_synth
+FREQ_MHZ := 150.0
 
 hw_verilog:
 	$(SBT) $(SBT_FLAGS) "runMain rosetta.ChiselMain --backend v --targetDir $(BUILD_DIR_VERILOG)"
@@ -14,6 +23,11 @@ hw_cpp:
 	$(SBT) $(SBT_FLAGS) "runMain rosetta.ChiselMain --backend c --targetDir $(BUILD_DIR_HWCPP)"
 
 hw_driver:
-	mkdir -p "$(BUILD_DIR_HWDRV)"; $(SBT) $(SBT_FLAGS) "runMain rosetta.DriverMain $(BUILD_DIR_HWDRV)"
+	mkdir -p "$(BUILD_DIR_HWDRV)"; $(SBT) $(SBT_FLAGS) "runMain rosetta.DriverMain $(BUILD_DIR_HWDRV) $(DRV_SRC_DIR)"
 
-.PHONY: hw_verilog
+hw_vivadoproj: hw_verilog
+	vivado -mode batch -source $(VIVADO_PROJ_SCRIPT) -tclargs $(TOP) $(HW_VERILOG) $(BITFILE_PRJNAME) $(BITFILE_PRJDIR) $(FREQ_MHZ)
+
+
+
+.PHONY: hw_verilog hw_cpp hw_driver hw_vivadoproj
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ package rosetta
 import Chisel._
 import fpgatidbits.PlatformWrapper.PYNQWrapper
 import fpgatidbits.Testbenches.TestRegOps
+import sys.process._
 
 object Settings {
   // Rosetta will use myInstFxn to instantiate your accelerator
@@ -19,10 +20,28 @@ object ChiselMain {
 }
 
 // call this object's main method to generate the register driver for your
-// accelerator. specify the target directory as the first argument.
+// accelerator. expects the following command line arguments, in order:
+// 1. path to output directory for generated files
+// 2. path to Rosetta drivers
 object DriverMain {
+  // utility functions to copy files inside Scala
+  def fileCopy(from: String, to: String) = {
+    s"cp -f $from $to" !
+  }
+
+  def fileCopyBulk(fromDir: String, toDir: String, fileNames: Seq[String]) = {
+    for(f <- fileNames)
+      fileCopy(s"$fromDir/$f", s"$toDir/$f")
+  }
+
   def main(args: Array[String]): Unit = {
+    val outDir = args(0)
+    val drvSrcDir = args(1)
+    // instantiate the wrapper accelerator
     val myModule = Module(new PYNQWrapper(Settings.myInstFxn))
-    myModule.generateRegDriver(args(0))
+    // generate the register driver
+    myModule.generateRegDriver(outDir)
+    // copy additional driver files
+    fileCopyBulk(drvSrcDir, outDir, myModule.platformDriverFiles)
   }
 }
